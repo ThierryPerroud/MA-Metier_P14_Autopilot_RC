98f4a31a8afa48f1a75bd72d615ef37988b051b6
Jason_dames <pn68ipb@eduvaud.ch>
2026-01-16 10:41:50 +0100

    Fichier init

---
+++ Class/__init__.py
@@ -0,0 +1,17 @@
+from database import *
+from Configurations import *
+from Flights import *
+from Thermals import *
+from Gliders import *
+from Telemetry_measurements import *
+from gliders_has_flights import *
+
+__all__ = [
+    database,
+    Configurations,
+    Flights,
+    Thermals,
+    Gliders,
+    Telemetry_measurements,
+    gliders_has_flights
+]
