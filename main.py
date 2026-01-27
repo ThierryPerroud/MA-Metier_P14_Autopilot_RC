c71c0ea20a9875db084fcbf4d54e16a59e35cb62
pt52obq <pt52obq@eduvaud.ch>
2026-01-27 14:07:34 +0100

    Modified GetData.py to export database data to a csv file at the end of main.py, added most units to main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from SimConnect import SimConnect, AircraftRequests
 import time
+import math
 from Class.database import SessionLocal
 from CRUD import create_measurement, create_flight, update_flight
 from datetime import datetime
 from Calculations import get_wind_direction, get_wind_speed, get_pressure
+from ExportData.GetData import get_data
 
 def insert_measurement(db, flight_id):
-    latitude = aq.get("PLANE_LATITUDE")
-    longitude = aq.get("PLANE_LONGITUDE")
-    altitude = aq.get("PLANE_ALTITUDE")
-    altitude_agl = aq.get("PLANE_ALT_ABOVE_GROUND")
-    indicated_airspeed = aq.get("AIRSPEED_INDICATED")
-    ground_speed = aq.get("GROUND_VELOCITY")
-    vertical_speed = aq.get("VERTICAL_SPEED")
-    pitch = aq.get("PLANE_PITCH_DEGREES")
-    roll = aq.get("PLANE_BANK_DEGREES")
-    yaw = aq.get("PLANE_HEADING_DEGREES_MAGNETIC")
-    vario = aq.get("VARIOMETER_RATE")
-    g_force = aq.get("G_FORCE")
-    wind_direction = get_wind_direction(aq.get("AIRCRAFT_WIND_X"), aq.get("AIRCRAFT_WIND_Z"),
+    latitude = aq.get("PLANE_LATITUDE")                                                                             # degrees
+    longitude = aq.get("PLANE_LONGITUDE")                                                                           # degrees
+    altitude = aq.get("PLANE_ALTITUDE")                                                                             # feet
+    altitude_agl = aq.get("PLANE_ALT_ABOVE_GROUND")                                                                 # feet
+    indicated_airspeed = aq.get("AIRSPEED_INDICATED")                                                               # knots/s
+    ground_speed = aq.get("GROUND_VELOCITY")                                                                        # knots/s
+    vertical_speed = aq.get("VERTICAL_SPEED")                                                                       # TBD
+    pitch = math.degrees(aq.get("PLANE_PITCH_DEGREES"))                                                             # degrees
+    roll = math.degrees(aq.get("PLANE_BANK_DEGREES"))                                                               # degrees
+    yaw = math.degrees(aq.get("PLANE_HEADING_DEGREES_MAGNETIC"))                                                    # degrees
+    vario = aq.get("VARIOMETER_RATE")                                                                               # TBD
+    g_force = aq.get("G_FORCE")                                                                                     # TBD
+    wind_direction = get_wind_direction(aq.get("AIRCRAFT_WIND_X"), aq.get("AIRCRAFT_WIND_Z"),                       # degrees
                                         aq.get("PLANE_HEADING_DEGREES_MAGNETIC"))
-    wind_force = get_wind_speed(aq.get("AIRCRAFT_WIND_X"), aq.get("AIRCRAFT_WIND_Y"), aq.get("AIRCRAFT_WIND_Z"))
-    temperature = aq.get("TOTAL_AIR_TEMPERATURE")
-    pressure = get_pressure(aq.get("SEA_LEVEL_PRESSURE"), aq.get("PLANE_ALTITUDE"))
+    wind_force = get_wind_speed(aq.get("AIRCRAFT_WIND_X"), aq.get("AIRCRAFT_WIND_Y"), aq.get("AIRCRAFT_WIND_Z"))    # m/s
+    temperature = aq.get("TOTAL_AIR_TEMPERATURE")                                                                   # degrees Celsius
+    pressure = get_pressure(aq.get("SEA_LEVEL_PRESSURE"), aq.get("PLANE_ALTITUDE"))                                 # atmosphere
 
     create_measurement(db, datetime.now(), latitude, longitude, altitude, altitude_agl, indicated_airspeed, ground_speed,
                        vertical_speed, pitch, roll, yaw, vario, g_force, wind_direction, wind_force, temperature,
@@ -32,6 +34,7 @@ def insert_measurement(db, flight_id):
 sm = SimConnect()
 aq = AircraftRequests(sm)
 db = SessionLocal()
+
 no_sim = False
 
 try:
@@ -55,4 +58,5 @@ while not no_sim:
         print("Impossible d'enregistrer les données du vol, le simulateur n'est plus en marche")
         no_sim = True
 
+get_data(current_flight)
 sm.exit()
